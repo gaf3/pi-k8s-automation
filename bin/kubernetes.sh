db23c7215f05d25660af309c1631534a1cf6370c
Gaffer Fitch <gaffer.fitch@gmail.com>
2018-11-04 15:14:32 -0500

    Lock version

--- bin/kubernetes.sh
+++ bin/kubernetes.sh
@@ -18,4 +18,4 @@ echo $orig | sudo tee /boot/cmdline.txt
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
   echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
   sudo apt-get update -q && \
-sudo apt-get install -qy kubeadm
+sudo apt-get install -qy kubeadm=1.10.9
